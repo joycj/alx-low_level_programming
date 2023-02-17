6ac3482c7798608f442944e0377ad1cc09d02193
joycj <okekechinenye90@gmail.com>
2023-02-17 02:37:50 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print letters without e and q
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
